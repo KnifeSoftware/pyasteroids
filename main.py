d9e339f4970aa3fb8ecada3e247a937866ee52a3
knifeSoftware <knifeSoftware@gmail.com>
2024-10-15 13:13:40 -0400

    B: Triangle on center screen

--- main.py
+++ main.py
@@ -2,30 +2,30 @@
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+from player import Player
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
-    loop_bool = True
-
-    while loop_bool == True:
+    while True:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((128, 0, 0))
+        screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+    
+        game_clock.tick(60)
+        dt = game_clock.get_time() / 1000   
+        print(dt)
 
 if __name__ == "__main__":
     main()
