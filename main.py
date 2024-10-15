8bbb8fcbaca17d9b7a40ebdee64347b748f8e92c
knifeSoftware <knifeSoftware@gmail.com>
2024-10-15 19:55:24 -0400

    D: Asteroid Field

--- main.py
+++ main.py
@@ -5,11 +5,24 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
@@ -18,16 +31,20 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-        player.update(dt)
+        for obj in updatable:
+            obj.update(dt)
 
         screen.fill((0, 0, 0))
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
     
         game_clock.tick(60)
         dt = game_clock.get_time() / 1000   
-        print(dt)
+        # limit the framerate to 60 FPS
+        #dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
